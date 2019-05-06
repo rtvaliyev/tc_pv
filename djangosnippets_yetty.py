51b52aa430888f8694164575f44cf7a095a9d2d5
Ramil Valili <50237743+ramilvalili@users.noreply.github.com>
2019-05-06 11:03:47 +0400

    Update and rename Script_by_yetty.py to djangosnippets_yetty.py

--- djangosnippets_yetty.py
+++ djangosnippets_yetty.py
@@ -23,32 +23,3 @@ class PlainTextPassword(BasePasswordHasher):
             (_('iterations'), iterations),
             (_('password'), password),
         ])
-
-
-
-
-
-
-
-
-
-# from django.contrib.auth.hashers import BasePasswordHasher
-
-# class PlainTextPassword(BasePasswordHasher):
-#     algorithm = "plain"
-
-#     def salt(self):
-#         return ''
-
-#     def encode(self, password, salt):
-#         assert salt == ''
-#         return password
-
-#     def verify(self, password, encoded):
-#         return password == encoded
-
-#     def safe_summary(self, encoded):
-#         return OrderedDict([
-#             (_('algorithm'), self.algorithm),
-#             (_('hash'), encoded),
-#         ])
